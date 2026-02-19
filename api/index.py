5ecbcbc22868a9b517d81fd2e19d75b94774c5fb
Karan <karan@Karans-MacBook-Air.local>
2026-02-18 21:46:36 -0600

    fix import order in api/index.py: set sys.path before import

--- api/index.py
+++ api/index.py
@@ -1,8 +1,9 @@
 import sys
 import os
-from f1_backend import create_app
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+from f1_backend import create_app
 
 app = create_app()
 
